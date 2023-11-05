e6cee3a5ae53c21d9edcdf6f7e20f6a030c115cf
FelipeSnJose <snfelipe96@gmail.com>
2023-11-05 03:10:44 -0300

    add try catch

--- utils/server.js
+++ utils/server.js
@@ -7,34 +7,54 @@ const { getCanciones, agregarCancion, eliminarCancion, actualizarCancion } = req
 app.use(express.json())
 
 app.get('/', (_, res) => {
+  try {
     const file = fs.readFileSync('./public/index.html', 'UTF-8')
     res.status(200).end(file)
-  })
+  } catch (error) {
+    res.status(500).json({ error: 'Error al leer el archivo HTML' })
+  }
+})
 
-  app.get('/canciones', (_, res) => {
+app.get('/canciones', (_, res) => {
+  try {
     const canciones = getCanciones()
     res.json(canciones)
-  })
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener la lista de canciones' })
+  }
+})
 
-  app.post('/canciones', (req, res) => {
+app.post('/canciones', (req, res) => {
+  try {
     const cancion = req.body
     agregarCancion(cancion)
     res.send('Canción agregada con éxito!')
-  })
+  } catch (error) {
+    res.status(500).json({ error: 'Error al agregar la canción' })
+  }
+})
 
-  app.delete('/canciones/:id', (req, res) => {
+app.delete('/canciones/:id', (req, res) => {
+  try {
     const { id } = req.params
     eliminarCancion(id)
     res.send('Canción eliminada con éxito')
-  })
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar la canción' })
+  }
+})
 
-  app.put('/canciones/:id', (req, res) => {
+app.put('/canciones/:id', (req, res) => {
+  try {
     const { id } = req.params
     const nuevaCancion = req.body
     actualizarCancion(id, nuevaCancion)
     res.send('Canción modificada con éxito')
-  })
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar la canción' })
+  }
+})
 
-app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'error 404 not found' }))
+app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'Error 404: Recurso no encontrado' }))
 
 app.listen(PORT, () => console.log(`Servidor OK: http://localhost:${PORT}`))
